5eb6ebd17b59b5e23835e52614d4bea349665ec0
Saeedmaldosary <saeedmaldosary@gmail.com>
2021-01-06 20:11:51 +0300

    Fix comments

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ driver.get("https://web.whatsapp.com/")
 
 time.sleep(10)
 searchbox = driver.find_element_by_xpath("//div[@id='side']/div[1]/div/label/div/div[2]")
-//Enter the name of the person you want to send the message to
+#Enter the name of the person you want to send the message to
 searchbox.send_keys('person name')
 searchbox.send_keys(Keys.ENTER)
 time.sleep(10)
 inputbox = driver.find_element_by_xpath("//div[@id='main']/footer/div[1]/div[2]/div/div[2]")
-//Enter the the message that you want to be sent
+#Enter the the message that you want to be sent
 inputbox.send_keys('message')
 inputbox.send_keys(Keys.ENTER)
